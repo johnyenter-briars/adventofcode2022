6620ab3c56f7b08409096489645d2916c06fff34
John Yenter-Briars <jyenterbriars@gmail.com>
2022-12-06 08:24:12 -0600

    more clippy

--- src/day4/campcleanup.rs
+++ src/day4/campcleanup.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use crate::util::reading::read_lines;
 
 pub fn overlapping_full_pairs_part1(path: &str) -> Result<String, Box<dyn Error>> {
-    let pairs = read_lines(path, true)?
+    Ok(read_lines(path, true)?
         .iter()
         .filter(|s| !s.is_empty())
         .filter_map(|line| {
@@ -24,21 +24,20 @@ pub fn overlapping_full_pairs_part1(path: &str) -> Result<String, Box<dyn Error>
             let pair2_l: i32 = pair2_l.parse().unwrap();
             let pair2_u: i32 = pair2_u.parse().unwrap();
 
-            if pair2_l <= pair1_l && pair1_u <= pair2_u {
-                Some(true)
-            } else if pair1_l <= pair2_l && pair2_u <= pair1_u {
+            if (pair2_l <= pair1_l && pair1_u <= pair2_u)
+                || (pair1_l <= pair2_l && pair2_u <= pair1_u)
+            {
                 Some(true)
             } else {
                 None
             }
         })
-        .collect::<Vec<bool>>();
-
-    Ok(pairs.len().to_string())
+        .count()
+        .to_string())
 }
 
 pub fn overlapping_part_of_pairs_part2(path: &str) -> Result<String, Box<dyn Error>> {
-    let pairs = read_lines(path, true)?
+    Ok(read_lines(path, true)?
         .iter()
         .into_iter()
         .filter_map(|line| {
@@ -59,19 +58,16 @@ pub fn overlapping_part_of_pairs_part2(path: &str) -> Result<String, Box<dyn Err
             let pair2_l: i32 = pair2_l.parse().unwrap();
             let pair2_u: i32 = pair2_u.parse().unwrap();
 
-            if pair2_l <= pair1_l && pair1_l <= pair2_u {
-                Some(true)
-            } else if pair2_l <= pair1_u && pair1_u <= pair2_u {
-                Some(true)
-            } else if pair1_l <= pair2_u && pair2_u <= pair1_u {
-                Some(true)
-            } else if pair1_l <= pair2_l && pair2_l <= pair1_u {
+            if (pair2_l <= pair1_l && pair1_l <= pair2_u)
+                || (pair2_l <= pair1_u && pair1_u <= pair2_u)
+                || (pair1_l <= pair2_u && pair2_u <= pair1_u)
+                || (pair1_l <= pair2_l && pair2_l <= pair1_u)
+            {
                 Some(true)
             } else {
                 None
             }
         })
-        .collect::<Vec<bool>>();
-
-    Ok(pairs.len().to_string())
+        .count()
+        .to_string())
 }
